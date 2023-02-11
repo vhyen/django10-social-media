72a870491a3dcba835fb19dbe2cbe73b5b3bcfc9
Rancon1 <blazeroid.of@gmail.com>
2023-02-11 14:38:42 +0700

    fixed synthax error

--- images/admin.py
+++ images/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 
 from .models import Image
 
-@admin_register(Image)
+@admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'image', 'created']
     list_filter = ['created']
-    
