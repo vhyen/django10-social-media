72312f6bd45e203da7666f927174359a896ee1eb
vhyen <vanhoangyen0206@gmail.com>
2023-03-29 22:50:42 +0700

    update default url

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+    path('', include('account.urls')),
     path('social-auth/',
     include('social_django.urls', namespace='social')),
     path('images/', include('images.urls', namespace='images')),
